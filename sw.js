95412f637817c6ad16e8b09840ffc2fcfdfa86e9
Waleed <tiktak5757@gmail.com>
2026-06-03 04:08:49 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Self-destruct - clear all caches and unregister
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.registration.unregister())
-  );
-});
